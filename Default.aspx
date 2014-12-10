40d7f5bf83d7e56db1c47983bf8995e2561a7d07
mouxideatest <nshdennis@gmail.com>
2014-12-10 22:45:58 +0800

    test
    test

---
+++ Default.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register Assembly="Infragistics4.Web.v14.2, Version=14.2.20142.1028, Culture=neutral, PublicKeyToken=7dd5c3163f2cd0cb"
+    Namespace="Infragistics.Web.UI.GridControls" TagPrefix="ig" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript" id="igClientScript">
+<!--
+     
+        function addChildRow() {
+            var grid = $find("WebHierarchicalDataGrid1");
+            var childBand = grid.get_gridView().get_rows().get_row(grid.get_gridView().get_rows().get_length() - 1).get_rowIslands()[0];
+            var childRow = new Array("", "text", "12");
+            childBand.get_rows().add(childRow);
+        }
+        function ajaxResponse(sender, e) {
+            var grid = $find("WebHierarchicalDataGrid1");
+            if (sender.get_id() == grid.get_gridView().get_id()) {    
+                setTimeout("addChildRow();", 200);
+            }
+                    }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        <ig:WebHierarchicalDataGrid ID="WebHierarchicalDataGrid1" runat="server" Height="350px"
+            Width="400px" DataKeyFields="ID" AutoGenerateColumns="False"  EnableAjax="true"
+            AutoGenerateBands="False" InitialDataBindDepth="-1" InitialExpandDepth="-1" 
+            onrowadding="WebHierarchicalDataGrid1_RowAdding">
+            <ClientEvents  AJAXResponse="ajaxResponse"/>
+            <Columns>
+                <ig:BoundDataField DataFieldName="ID" Key="ID">
+                    <Header Text="ID" />
+                </ig:BoundDataField>
+                <ig:BoundDataField DataFieldName="Name" Key="Name">
+                    <Header Text="Name" />
+                </ig:BoundDataField>
+                <ig:BoundDataField DataFieldName="Sum" Key="Sum">
+                    <Header Text="Sum" />
+                </ig:BoundDataField>
+            </Columns>
+            <Bands>
+                <ig:Band DataMember="Child" AutoGenerateColumns="false">
+                    <Columns>
+                        <ig:BoundDataField DataFieldName="ID" Key="ID">
+                            <Header Text="ID" />
+                        </ig:BoundDataField>
+                        <ig:BoundDataField DataFieldName="Address" Key="Address">
+                            <Header Text="Address" />
+                        </ig:BoundDataField>
+                        <ig:BoundDataField DataFieldName="Count" Key="Count">
+                            <Header Text="Count" />
+                        </ig:BoundDataField>
+                    </Columns>
+                </ig:Band>
+            </Bands>
+            <Behaviors>
+                <ig:EditingCore EnableInheritance="true" AutoCRUD="false" >
+                    <Behaviors>
+                        <ig:RowAdding EnableInheritance="true">
+                        </ig:RowAdding>
+                    </Behaviors>
+                </ig:EditingCore>
+            </Behaviors>
+        </ig:WebHierarchicalDataGrid>       
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["data"] == null)
+        {
+            Session["data"] = populateDataSet();
+        }
+        WebHierarchicalDataGrid1.DataSource = (DataSet)Session["data"];
+        WebHierarchicalDataGrid1.DataBind();
+    }
+
+    private DataSet populateDataSet()
+    {
+
+        DataSet ds = new DataSet();
+        DataTable Parent = new DataTable();
+        Parent.TableName = "Parent";
+        DataTable Child = new DataTable();
+        Child.TableName = "Child";
+
+        Parent.Columns.Add("ID");
+        Parent.Columns.Add("Name");
+
+        DataColumn[] col = new DataColumn[1];
+        col[0] = Parent.Columns["ID"];
+        Parent.PrimaryKey = col;
+
+        DataRow row = Parent.NewRow();
+        row.SetField<int>("ID", 0);
+        row.SetField<string>("Name", "Alice");
+        Parent.Rows.Add(row);
+
+        row = Parent.NewRow();
+        row.SetField<int>("ID", 1);
+        row.SetField<String>("Name", "Bob");
+        Parent.Rows.Add(row);
+
+
+        Child.Columns.Add("ID");
+        Child.Columns.Add("Address");
+        Child.Columns.Add("Count", typeof(int));
+
+        row = Child.NewRow();
+        row.SetField<int>("ID", 0);
+        row.SetField<string>("Address", "Paris");
+        row.SetField<int>("Count", 5);
+        Child.Rows.Add(row);
+
+        row = Child.NewRow();
+        row.SetField<int>("ID", 1);
+        row.SetField<string>("Address", "Rome");
+        row.SetField<int>("Count", 9);
+        Child.Rows.Add(row);
+
+        row = Child.NewRow();
+        row.SetField<int>("ID", 1);
+        row.SetField<string>("Address", "London");
+        row.SetField<int>("Count", 2);
+        Child.Rows.Add(row);
+
+        row = Child.NewRow();
+        row.SetField<int>("ID", 0);
+        row.SetField<string>("Address", "LosAngeles");
+        row.SetField<int>("Count", 9);
+        Child.Rows.Add(row);
+
+        ds.Tables.Add(Parent);
+        ds.Tables.Add(Child);
+
+
+        DataRelation relation = new DataRelation("IDRelation", Parent.Columns["ID"], Child.Columns["ID"]);
+        ds.Relations.Add(relation);
+
+
+        // Add the column in the parent DataTable which Sums the values from the children based on the id relation
+        Parent.Columns.Add("Sum", typeof(int), "Sum(Child(IDRelation).Count)");
+
+        return ds;
+    }
+    protected void WebHierarchicalDataGrid1_RowAdding(object sender, Infragistics.Web.UI.GridControls.RowAddingEventArgs e)
+    {
+
+        if (((Infragistics.Web.UI.GridControls.ContainerGrid)(sender)).Level == 0)
+        {
+            //parent
+            DataSet parentTable = (DataSet)Session["data"];
+            var row = parentTable.Tables["Parent"].NewRow();
+
+            row["ID"] = e.Values["ID"];
+            row["Name"] = e.Values["Name"];
+
+            parentTable.Tables["Parent"].Rows.Add(row);
+
+            WebHierarchicalDataGrid1.DataSource = parentTable;
+            WebHierarchicalDataGrid1.DataBind();
+
+            Session["data"] = parentTable;
+        }
+            //child
+        else
+        {
+            DataSet childTable = (DataSet)Session["data"];
+            var row = childTable.Tables["Child"].NewRow();
+
+            row["ID"] = this.WebHierarchicalDataGrid1.Rows[this.WebHierarchicalDataGrid1.Rows.Count - 1].Items[0].Value;
+            row["Address"] = e.Values["Address"];
+            row["Count"] = e.Values["Count"];
+
+            childTable.Tables["Child"].Rows.Add(row);
+
+            WebHierarchicalDataGrid1.DataSource = childTable;
+            WebHierarchicalDataGrid1.DataBind();
+
+            Session["data"] = childTable;
+        }
+       
+
+       
+    }
+}
